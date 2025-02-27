dc5bb926fbbe64964b690ff2b81aa51816deafe1
Jared <jaredtponting@gmail.com>
2025-02-27 17:28:56 +0000

    Touchscreen changes

--- create_flashcards.py
+++ create_flashcards.py
@@ -3,6 +3,24 @@ from app import create_app, db
 from app.models import Flashcard
 
 
+# with app.app_context():
+#     db.drop_all()  # This will drop all tables in the database
+#     db.create_all()  # Recreate them
+
+# app.app_context().push()
+
+# with open('app/static/csv/A1_word_list.csv', newline='', encoding='utf-8') as csvfile:
+#     reader = csv.DictReader(csvfile)
+#     for row in reader:
+#         card = Flashcard(
+#             german_text=row['Question'],
+#             english_translation=row['Answer']
+#         )
+#         db.session.add(card)
+#     db.session.commit()
+#
+# print("A1 flashcards loaded successfully!")
+
 if __name__ =="__main__":
     app = create_app()
     app.app_context().push()
